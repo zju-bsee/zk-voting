488d7794ccbf734c1c786d26fc0534a12b24a144
aeof <30540276+dialtcp@users.noreply.github.com>
2022-04-02 17:09:04 +0800

    Feat(JNI): 实现基本JNI接口.

--- src/jni/cn/edu/zjucst/jni/ZKVotingJNI.java
+++ src/jni/cn/edu/zjucst/jni/ZKVotingJNI.java
@@ -3,80 +3,68 @@ package cn.edu.zjucst.jni;
 import java.math.BigInteger;
 
 public class ZKVotingJNI {
+    private Key key;
+    private BigInteger[] voterIDs;
+
     // 引入lib-zkvoting.so
     static {
         System.loadLibrary("zkvoting");
     }
 
-    // 根据选举选民数产生身份证明电路对应key
-    // private static native int generateIdentityKeys(int voterNum);
-    public static native int hello(int i);
+    // 为了避免重复调用JNI生成命题，验证前需要实例化一个ZKVotingJNI对象
+    // TODO: 当前电路voterIDs只能传int范围，电路完善后传sha256的hash值
+    public ZKVotingJNI(Key key, BigInteger[] voterIDs) {
+        this.key = key;
+        this.voterIDs = voterIDs;
+    }
 
-    public static int GenerateVoterKeys(int voterNum) {
-        // return generateIdentityKeys(voterNum);
-        return voterNum;
+    // Note: 相同数量的选民数电路相同，但是多次调用会产生不同的key
+    private static native Key generateVoterKeys(int voterNum);
+
+    /**
+     * 生成选民的密钥
+     * @param voterNum 选民数量
+     * @return 验证者、证明者密钥
+     */
+    public static Key GenerateVoterKeys(int voterNum) {
+        if (voterNum <= 1) {
+            return null;
+        }
+        return generateVoterKeys(voterNum);
+    }
+
+    private native boolean verifyVoterProof(Proof proof);
+
+    /**
+     * 验证选民的证明
+     * @param proof 选民的证明
+     * @return 验证通过返回true，否则返回false
+     */
+    public boolean VerifyVoterProof(Proof proof) {
+        if (this.voterIDs.length <= 2) {
+            return false;
+        }
+        return verifyVoterProof(proof);
     }
 
     public static void main(String[] args) {
-        System.out.println(GenerateVoterKeys(100));
+        // 为选民数3的选举活动产生key
+        Key key = GenerateVoterKeys(3);
+        System.out.println(key);
+
+        // 测试选举proof验证
+        BigInteger[] voterIDs = new BigInteger[3];
+        voterIDs[0] = new BigInteger("1");
+        voterIDs[1] = new BigInteger("2");
+        voterIDs[2] = new BigInteger("3");
+
+        System.out.println(new ZKVotingJNI(key, voterIDs).VerifyVoterProof(null));
     }
-
-    // // 根据候选人数目产生电路对应key
-    // public static Key GenerateBallotKeys(int candidateNum) {
-    // byte[] keyBytes = generateBallotKeys(candidateNum);
-    // return new Key(keyBytes);
-    // }
-
-    // // 根据(选民哈希列表，选民哈希)产生选举证明
-    // private static Proof GenerateVotingProof(ProvingKey provingKey, byte[][]
-    // idHashs, byte[] idHash) {
-    // byte[] proofBytes = generateVotingProof(provingKey.serialize(), idHashs,
-    // idHash);
-    // return new Proof(proofBytes);
-    // }
-
-    // // 根据(候选人数，选票)产生选票证明
-    // private static Proof GenerateBallotProof(ProvingKey provingKey, int
-    // candidateNum, byte[] ballot) {
-    // byte[] proofBytes = generateBallotProof(provingKey.serialize(), candidateNum,
-    // ballot);
-    // return new Proof(proofBytes);
-    // }
-
-    // // 根据(验证密钥，选举人证明，选民哈希列表)验证选举人证明
-    // private static boolean VerifyVotingProof(VerifyingKey verifyingKey, Proof
-    // proof, byte[][] idHashs) {
-    // byte[] verifyingKeyBytes = verifyingKey.serialize();
-    // byte[] proofBytes = proof.serialize();
-    // return verifyVotingProof(verifyingKeyBytes, proofBytes, idHashs);
-    // }
-
-    // // 根据（验证密钥，选票证明，）
-    // private static boolean VerifyBallotProof(VerifyingKey verifyingKey, Proof
-    // proof) {
-    // byte[] verifyingKeyBytes = verifyingKey.serialize();
-    // byte[] proofBytes = proof.serialize();
-    // return verifyBallotProof(verifyingKeyBytes, proofBytes);
-    // }
-
-    // private static native byte[] generateBallotKeys(int candidateNum);
-
-    // private static native byte[] generateVotingProof(byte[] provingKey, byte[][]
-    // idHashs, byte[] idHash);
-
-    // private static native byte[] generateBallotProof(byte[] provingKey, int
-    // candidateNum, byte[] ballot);
-
-    // private static native boolean verifyVotingProof(byte[] verifyingKey, byte[]
-    // proof, byte[][] idHashs);
-
-    // private static native boolean verifyBallotProof(byte[] verifyingkey, byte[]
-    // proof);
 }
 
 class Key {
-    private final ProvingKey provingKey;
-    private final VerifyingKey verifyingKey;
+    private ProvingKey provingKey;
+    private VerifyingKey verifyingKey;
 
     public Key(ProvingKey provingKey, VerifyingKey verifyingKey) {
         this.provingKey = provingKey;
@@ -94,14 +82,14 @@ class Key {
 
 class ProvingKey {
     // TODO: 动态字段还未完整
-    public final G1 alpha_g1;
-    public final G1 beta_g1;
-    public final G2 beta_g2;
-    public final G1 delta_g1;
-    public final G2 delta_g2;
+    public G1 alpha;
+    public G1 beta_g1;
+    public G2 beta_g2;
+    public G1 delta_g1;
+    public G2 delta_g2;
 
     public ProvingKey(G1 alpha_g1, G1 beta_g1, G2 beta_g2, G1 delta_g1, G2 delta_g2) {
-        this.alpha_g1 = alpha_g1;
+        this.alpha = alpha_g1;
         this.beta_g1 = beta_g1;
         this.beta_g2 = beta_g2;
         this.delta_g1 = delta_g1;
@@ -111,7 +99,7 @@ class ProvingKey {
     @Override
     public String toString() {
         return "{" +
-                " alpha_g1='" + alpha_g1 + "'" +
+                " alpha_g1='" + alpha + "'" +
                 ", beta_g1='" + beta_g1 + "'" +
                 ", beta_g2='" + beta_g2 + "'" +
                 ", delta_g1='" + delta_g1 + "'" +
@@ -121,11 +109,11 @@ class ProvingKey {
 }
 
 class VerifyingKey {
-    public final G1 alpha;
-    public final G2 beta;
-    public final G2 gamma;
-    public final G2 delta;
-    public final G1[] gamma_abc;
+    public G1 alpha;
+    public G2 beta;
+    public G2 gamma;
+    public G2 delta;
+    public G1[] gamma_abc;
 
     public VerifyingKey(G1 alpha, G2 beta, G2 gamma, G2 delta, G1[] gamma_abc) {
         this.alpha = alpha;
@@ -160,8 +148,8 @@ class Proof {
 }
 
 class G1 {
-    public final BigInteger x;
-    public final BigInteger y;
+    public BigInteger x;
+    public BigInteger y;
 
     public G1(BigInteger x, BigInteger y) {
         this.x = x;
@@ -178,10 +166,10 @@ class G1 {
 }
 
 class G2 {
-    public final BigInteger x0;
-    public final BigInteger x1;
-    public final BigInteger y0;
-    public final BigInteger y1;
+    public BigInteger x0;
+    public BigInteger x1;
+    public BigInteger y0;
+    public BigInteger y1;
 
     public G2(BigInteger x0, BigInteger x1, BigInteger y0, BigInteger y1) {
         this.x0 = x0;
