da5cf1929c9a70a28140842279d3e59a56eaa85d
aeof <30540276+dialtcp@users.noreply.github.com>
2022-04-13 21:21:22 +0800

    Fix(Identity): 增加电路取模操作，避免乘积溢出.

--- src/identity_fake_gadget.hpp
+++ src/identity_fake_gadget.hpp
@@ -6,46 +6,66 @@
 
 #include "util.hpp"
 
+// It should be large or it can be easily cracked.
+const long long MOD = 1000000009;
+
 namespace identity {
 template <typename FieldT>
 class identity_gadget : public gadget<FieldT> {
    public:
-    // Voter num
+    // Voter count
     std::vector<long long> voters;
     // Private input
     pb_variable<FieldT> x;
     // Intermediate output
     pb_variable_array<FieldT> minus;
     pb_variable_array<FieldT> multiply;
+    pb_variable_array<FieldT> quotient;
+    pb_variable_array<FieldT> remainder;
 
-    identity_gadget(protoboard<FieldT> &pb, std::vector<long long> voter_ids)
-        : gadget<FieldT>(pb, "sha256_gadget"), voters(voter_ids) {
+    identity_gadget(protoboard<FieldT> &pb, std::vector<long long> &voter_ids)
+        : gadget<FieldT>(pb, "identity_gadget"), voters(voter_ids) {
         size_t num_voters = voters.size();
         if (num_voters <= 1) {
             return;
         }
 
         // Prover's id
-        x.allocate(pb, "prover-id");
+        x.allocate(pb, "prover id");
         // Minus intermediate variables: N
         minus.resize(num_voters);
+        std::cout << "Minus size: " << minus.size() << std::endl;
         minus.allocate(this->pb, num_voters, "minus-");
         // Multiply intermediate variables: N - 1
         multiply.resize(num_voters - 1);
-        multiply.allocate(this->pb, num_voters - 1, "multiply-");
+        multiply.allocate(this->pb, num_voters, "multiply-");
+        // Quotient intermediate variables: N - 1
+        quotient.resize(num_voters - 1);
+        quotient.allocate(this->pb, num_voters, "quotient-");
+        // Remainder intermediate variables: N - 1
+        remainder.resize(num_voters - 1);
+        remainder.allocate(this->pb, num_voters, "remainder-");
 
-        // generate_r1cs_constraint()
         // Minus
-        for (int i = 0; i < num_voters; i++) {
+        for (size_t i = 0; i < num_voters; i++) {
             this->pb.add_r1cs_constraint(
                 r1cs_constraint<FieldT>(x - voter_ids[i], 1, minus[i]));
         }
-        // Multiply
+
+        // Multiply: Handle the overflow!!!
+        // A * B % MOD = C
+        // quotient * MOD = A * B - C
+        // multiply = A * B
+        // quotient * MOD = multiply - C
         this->pb.add_r1cs_constraint(
             r1cs_constraint<FieldT>(minus[0], minus[1], multiply[0]));
-        for (int i = 1; i < num_voters - 1; i++) {
+        this->pb.add_r1cs_constraint(r1cs_constraint<FieldT>(
+            quotient[0], MOD, multiply[0] - remainder[0]));
+        for (size_t i = 1; i < num_voters - 1; i++) {
             this->pb.add_r1cs_constraint(r1cs_constraint<FieldT>(
-                multiply[i - 1], minus[i + 1], multiply[i]));
+                remainder[i - 1], minus[i + 1], multiply[i]));
+            this->pb.add_r1cs_constraint(r1cs_constraint<FieldT>(
+                quotient[i], MOD, multiply[i] - remainder[i]));
         }
         // Output
         this->pb.add_r1cs_constraint(
@@ -58,17 +78,29 @@ class identity_gadget : public gadget<FieldT> {
 
         // Prover
         this->pb.val(x) = id;
+
         // Minus
-        for (int i = 0; i < num_voters; i++) {
+        for (size_t i = 0; i < num_voters; i++) {
             this->pb.val(minus[i]) = id - voters[i];
             minusVals.push_back(id - voters[i]);
         }
+
         // Multiply
-        int prev_multiply = minusVals[0] * minusVals[1];
+        long long prev_multiply = minusVals[0] * minusVals[1];
+        long long prev_remainder = prev_multiply % MOD;
         this->pb.val(multiply[0]) = prev_multiply;
-        for (int i = 1; i < num_voters - 1; i++) {
-            prev_multiply = prev_multiply * minusVals[i + 1];
-            this->pb.val(multiply[i]) = prev_multiply;
+        this->pb.val(quotient[0]) = prev_multiply / MOD;
+        this->pb.val(remainder[0]) = prev_remainder;
+
+        for (size_t i = 1; i < num_voters - 1; i++) {
+            long long multiply_val = prev_remainder * minusVals[i + 1];
+
+            this->pb.val(multiply[i]) = multiply_val;
+            this->pb.val(quotient[i]) = multiply_val / MOD;
+            this->pb.val(remainder[i]) = multiply_val % MOD;
+
+            prev_multiply = multiply_val;
+            prev_remainder = multiply_val % MOD;
         }
     }
 };
--- src/test.cpp
+++ src/test.cpp
@@ -10,48 +10,38 @@ using namespace std;
 typedef default_r1cs_ppzksnark_pp ppT;
 typedef libff::Fr<default_r1cs_ppzksnark_pp> FieldT;
 
-int main(int argc, char* argv[]) {
+int main() {
     // Initialize the curve parameters.
-    default_r1cs_ppzksnark_pp::init_public_params();
+    ppT::init_public_params();
 
+    // Create a new protoboard.
     protoboard<FieldT> pb;
     vector<long long> vec_voter_ids;
-    for (int i = 0; i < 100; i++) {
-        vec_voter_ids.push_back(i);
+    for (int i = 0; i < 5000; i++) {
+        vec_voter_ids.push_back(i + 1);
     }
     identity::identity_gadget<FieldT> g(pb, vec_voter_ids);
 
+    // Generate keys.
+    std::cout << "==============================" << std::endl;
     auto cs = pb.get_constraint_system();
-    auto keypair = r1cs_ppzksnark_generator<default_r1cs_ppzksnark_pp>(cs);
+    auto keypair = r1cs_ppzksnark_generator<ppT>(cs);
 
     long long id_input;
+    std::cout << "Input your voter id to prove: ";
     std::cin >> id_input;
     g.generate_r1cs_witness(id_input);
 
     // Generate proof
-    auto proof = r1cs_ppzksnark_prover<default_r1cs_ppzksnark_pp>(
-        keypair.pk, pb.primary_input(), pb.auxiliary_input());
-    
+    std::cout << "==============================" << std::endl;
+    auto proof = r1cs_ppzksnark_prover<ppT>(keypair.pk, pb.primary_input(),
+                                            pb.auxiliary_input());
+
     // Verify the proof
-    bool is_valid =
-        r1cs_ppzksnark_verifier_strong_IC<default_r1cs_ppzksnark_pp>(
-            keypair.vk, pb.primary_input(), proof);
+    std::cout << "==============================" << std::endl;
+    bool is_valid = r1cs_ppzksnark_verifier_strong_IC<ppT>(
+        keypair.vk, pb.primary_input(), proof);
     std::cout << "is_valid: " << is_valid << std::endl;
-    
-    libff::bigint<128> a;
-    stringstream ss;
-    ss << "123804182308410238410238401238401823041823";
-    ss >> a;
-    std::cout << "a: " << a << std::endl;
-
-
-    auto str = any_to_string(keypair.pk);
-    istringstream in(str);
-    r1cs_ppzksnark_proving_key<ppT> pk;
-    in >> pk;
-    std::cout << (pk == keypair.pk) << std::endl;
-
-    std::cout << makeFq("3210923804182309841238410239841230841203841") << std::endl;
 
     return 0;
 }
